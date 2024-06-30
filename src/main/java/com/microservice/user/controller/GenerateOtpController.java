ef29a525c322e793eaa0d1ba47c96cf413d6f63e
Aryan Saini <aryan.saini@xebia.com>
2024-06-30 17:57:14 +0530

    generate Otp added

---
+++ src/main/java/com/microservice/user/controller/GenerateOtpController.java
@@ -0,0 +1,35 @@
+package com.microservice.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.user.exception.UserApplicationException;
+import com.microservice.user.service.GenerateOtpService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class GenerateOtpController {
+
+	private final GenerateOtpService generateOtpService;
+
+	@Autowired
+	public GenerateOtpController(GenerateOtpService generateOtpService) {
+		this.generateOtpService = generateOtpService;
+	}
+
+	@GetMapping("/generate-otp")
+	public ResponseEntity<String> resetPassword(@Valid @RequestParam String email)
+			throws UserApplicationException {
+
+		String response = generateOtpService.generateOtp(email);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+}
