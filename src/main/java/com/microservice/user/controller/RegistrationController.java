ed65c38cf6ec81363d54e83ca237c02364416fab
Aryan Saini <aryan.saini@xebia.com>
2024-06-30 16:40:07 +0530

    Registration and custom exception added

---
+++ src/main/java/com/microservice/user/controller/RegistrationController.java
@@ -0,0 +1,47 @@
+package com.microservice.user.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.entity.Users;
+import com.microservice.user.exception.UserApplicationException;
+import com.microservice.user.request.UserRegistrationRequestDto;
+import com.microservice.user.service.RegistrationService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+@RequestMapping("/api/v1/")
+@RestController
+public class RegistrationController {
+	
+	private final RegistrationService registrationService;
+	
+	@Autowired
+	public RegistrationController(RegistrationService registrationService) {
+		super();
+		this.registrationService = registrationService;
+	}
+	
+	@PostMapping("register")
+	public ResponseEntity<Users> registerUser(
+			@Valid @org.springframework.web.bind.annotation.RequestBody UserRegistrationRequestDto userRegistrationRequestDto) throws UserApplicationException {
+		Users response = registrationService.registerUser(userRegistrationRequestDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Users>> getAllUsers() {
+		List<Users> users = registrationService.getAllUsers();
+		return ResponseEntity.status(HttpStatus.OK).body(users);
+	}
+	
+
+}
---
+++ src/main/java/com/microservice/user/serviceImpl/RegistrationServiceImpl.java
@@ -0,0 +1,49 @@
+package com.microservice.user.serviceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.microservice.entity.Users;
+import com.microservice.user.dao.RegisterUserRepository;
+import com.microservice.user.exception.UserApplicationException;
+import com.microservice.user.request.UserRegistrationRequestDto;
+import com.microservice.user.service.RegistrationService;
+
+@Service
+public class RegistrationServiceImpl implements RegistrationService {
+
+	private final RegisterUserRepository registerUserRepository;
+	private final ModelMapper modelMapper;
+
+	@Autowired	
+	public RegistrationServiceImpl(RegisterUserRepository registerUserRepository, ModelMapper modelMapper) {
+		super();
+		this.modelMapper = modelMapper;
+		this.registerUserRepository = registerUserRepository;
+	}
+
+	@Override
+	public Users registerUser(UserRegistrationRequestDto userRegistrationRequestDto) throws UserApplicationException {
+		Optional<Users> optionalUser = registerUserRepository.findByEmail(userRegistrationRequestDto.getEmail());
+		if (optionalUser.isPresent()) {
+			throw new UserApplicationException(HttpStatus.BAD_REQUEST, "User already present");
+		}
+//		System.out.println("dto is:"+ userRegistrationRequestDto.getEmail());
+		Users user = modelMapper.map(userRegistrationRequestDto, Users.class);
+//		System.out.println("After mapping, user is: "+ user.getEmail());
+		registerUserRepository.save(user);
+		return user;
+	}
+
+	@Override
+	public List<Users> getAllUsers() {
+		return registerUserRepository.findAll();
+		
+	}
+
+}
