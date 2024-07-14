b4748367aa0b4d8c960108b9700498afef8f007f
Aryan Saini <aryan.saini@xebia.com>
2024-07-14 22:24:37 +0530

    apis modified

---
+++ src/main/java/com/onlineBanking/user/controller/CardController.java
@@ -0,0 +1,27 @@
+package com.onlineBanking.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.onlineBanking.user.request.DeactivateCardDto;
+import com.onlineBanking.user.service.CardService;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CardController {
+    @Autowired
+    private CardService cardService;
+    
+    // Deactivate Card on the basis of userId and last 4 digits of your card
+    @PostMapping("deactivate-card")
+    public String deactivateCard(@RequestParam Long userId, @RequestParam String last4Digits) {
+        return cardService.deactivateCard(userId, last4Digits);
+    }
+}
--- src/main/java/com/onlineBanking/user/util/ConstantUtil.java
+++ src/main/java/com/onlineBanking/user/util/ConstantUtil.java
@@ -7,5 +7,7 @@ public class ConstantUtil {
 	public static final String ACCOUNT_BLOCKED = "Account is Blocked. Try again after 24 hours.";
 	public static final String INVALID_CREDENTIALS = "Invalid Username or Password";
 	public static final String OTP_MISMATCH_EXCEEDED = "Invalid Otp (Exceeded 5 minutes)";
-	public static final String OTP_MISMATCH= "Invalid Otp";
+	public static final String OTP_MISMATCH = "Invalid Otp";
+	public static final String CREATE_ACCOUNT_API_URL = "http://localhost:8081/api/v1/create";
+	public static final String CARD_API_URL = "http://localhost:8082/api/v1/cards/deactivate";
 }
